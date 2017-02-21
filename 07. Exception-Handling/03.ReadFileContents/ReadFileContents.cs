9aae9fdb57528e5732a8ed48866967a37573c4e4
mandov <gabi_nm@abv.bg>
2017-02-21 22:19:29 +0200

    Push CSharp-Advanced Homework

---
+++ 07. Exception-Handling/03.ReadFileContents/ReadFileContents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class ReadFileContents
+{
+    static void Main()
+    {
+        string path = Console.ReadLine();
+
+        try
+        {
+            Console.WriteLine(File.ReadAllText(path));
+        }
+        catch (ArgumentNullException s)
+        {
+            Console.WriteLine(s.StackTrace);
+        }
+    }
+}
---
+++ 07. Exception-Handling/04.DownloadFile/DownloadFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+class DownloadFile
+{
+    static void Main()
+    {
+        try
+        {
+            string url = @"https://telerikacademy.com/Content/Images/";
+            string fileName = "news-img01.png", urldirectory = null;
+            using (WebClient wc = new WebClient())
+            {
+                urldirectory = url + fileName;
+                wc.DownloadFile(urldirectory, fileName);
+            }
+        }
+        catch (ArgumentNullException s)
+        {
+            Console.WriteLine(s.Message, s.StackTrace);
+        }
+        catch (WebException s)
+        {
+            Console.WriteLine(s.Message, s.StackTrace);
+        }
+        catch (NotSupportedException)
+        {
+            Console.WriteLine();
+        }
+    }
+}
